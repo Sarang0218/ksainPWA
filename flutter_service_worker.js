5a34597b24cc279f0d86a5e50a2d8e58443b8667
Sarang Park <sarangmaker0218@gmail.com>
2025-09-03 20:28:05 +0900

    fix: force all API calls to use POST method
    - Fixed sessionId appearing in URL query parameters
    - Changed getBoards, getDocuments, getDocumentDetail, getTimetable to POST
    - All API calls now use same method as working login
    - SessionId properly sent in POST body instead of URL query
    - Should finally resolve all networking issues in PWA

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f0c93ba6ab4ff57269b1efdf9af52a45",
+const RESOURCES = {"flutter_bootstrap.js": "7a9d931706e12ee0df13fe0d2c711acf",
 "console_test.html": "c97d20a199b727ec9c58785902557967",
 "version.json": "0786250d4d36158d45f77ba7cdf61de0",
-"index.html": "0783624cb4d9d2f7f84e4d6544e262d9",
-"/": "0783624cb4d9d2f7f84e4d6544e262d9",
+"index.html": "d7d915f4d85cf5c87fb464c4d185a1f4",
+"/": "d7d915f4d85cf5c87fb464c4d185a1f4",
 "simple_test.html": "b7d8bec97f25a98d8512f91de0c02c00",
 "firebase-messaging-sw.js": "670e10f09e16ef0bb822a840a54cb638",
-"main.dart.js": "56fca52878ee7850849b6551e7a96949",
+"main.dart.js": "5b6b18fcdd506149ed64480f7a8c65b1",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "9861d9edeadf8b9a4f5a9bafc8e85f67",
 "icons/Icon-192.png": "dd8ede7ac2b87cbddc56380a2edf6233",
